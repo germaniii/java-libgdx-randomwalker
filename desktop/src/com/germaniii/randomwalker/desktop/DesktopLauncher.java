037e4cfcdf19a04444d8c81afa5a2d56304751a9
German III <germaniiifelisarta@gmail.com>
2021-05-18 23:48:15 +0800

    v0.1 Added Desktop Config

--- desktop/src/com/germaniii/randomwalker/desktop/DesktopLauncher.java
+++ desktop/src/com/germaniii/randomwalker/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.germaniii.randomwalker.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Random Walker";
+		config.height = 800;
+		config.width = 600;
 		new LwjglApplication(new Main(), config);
 	}
 }
