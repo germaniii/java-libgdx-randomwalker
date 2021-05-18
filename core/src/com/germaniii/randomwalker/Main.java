a97cabb6c4c5512b0ce17c4ab0b901a34f0c54f0
German III <germaniiifelisarta@gmail.com>
2021-05-19 00:15:05 +0800

    v0.1 Added Random Motion

--- core/src/com/germaniii/randomwalker/Main.java
+++ core/src/com/germaniii/randomwalker/Main.java
@@ -1,31 +1,60 @@
 package com.germaniii.randomwalker;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer shapeRenderer;
+	Random rand = new Random();
+
+	float circleX = 200;
+	float circleY = 100;
+
+	float xSpeed = 60;
+	float ySpeed = 60;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear((float).5, (float).5, (float).5, 1);
+
+		// randomly move x, y
+		circleX += xSpeed * Gdx.graphics.getDeltaTime() * ((rand.nextInt(3)) - 1);
+		circleY += ySpeed * Gdx.graphics.getDeltaTime() * ((rand.nextInt(3)) - 1);
+
+		// Bounds conditions for X
+		if(circleX < 0){
+			circleX = 0;
+		}
+		if(circleX > Gdx.graphics.getWidth()){
+			circleX = Gdx.graphics.getWidth();
+		}
+		// Bounding conditions for Y
+		if(circleY < 0){
+			circleY = 0;
+		}
+		if(circleY > Gdx.graphics.getWidth()){
+			circleY = Gdx.graphics.getWidth();
+		}
+
+		// Render Circle
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(0, 1, 0, 1);
+		shapeRenderer.circle(circleX, circleY, 3);
+		shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/com/germaniii/randomwalker/desktop/DesktopLauncher.java
+++ desktop/src/com/germaniii/randomwalker/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Random Walker";
-		config.height = 800;
-		config.width = 600;
+		config.height = 600;
+		config.width = 800;
 		new LwjglApplication(new Main(), config);
 	}
 }
